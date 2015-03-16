823f17478a87bebdbaa806e18254cfde9d15d619
Roy Clarkson <roy@clarkson.org>
2015-03-16 17:26:17 -0500

    Add client and server

---
+++ client/src/main/java/demo/HomeController.java
@@ -0,0 +1,21 @@
+package demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+  private static final String template = "Hello, %s!";
+
+  @Value("${name:World}")
+  private String name;
+
+  @RequestMapping("/")
+  public String home() {
+    return String.format(template, name);
+  }
+
+}
